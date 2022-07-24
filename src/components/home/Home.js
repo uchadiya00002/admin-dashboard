7f00ff32324549de5d14547cf9883755d5a1bc5c
Avinash Uchadiya <avinashuchadiya786@gmail.com>
2022-07-24 18:32:32 +0530

    completed the homepage added the table

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,7 @@ import Chart from "../chart/Chart";
 import Featured from "../featured-chart/Featured";
 import Navbar from "../navbar/Navbar";
 import Sidebar from "../sidebar/Sidebar";
+import List from "../table/Table";
 import Widget from "../widget/Widget";
 import "./home.scss";
 
@@ -21,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <List />
         </div>
       </div>
     </div>
