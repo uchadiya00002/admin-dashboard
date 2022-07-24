d238de1968fc6b7ff0d0340cce6e120390cd76e0
Avinash Uchadiya <avinashuchadiya786@gmail.com>
2022-07-24 17:31:53 +0530

    added gradient chart on the homepage using rechart library

--- src/components/featured-chart/Featured.js
+++ src/components/featured-chart/Featured.js
@@ -2,7 +2,11 @@ import "./featured.scss";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-import { MoreVert } from "@mui/icons-material";
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreVert,
+} from "@mui/icons-material";
 import React from "react";
 
 const Featured = () => {
@@ -21,6 +25,29 @@ const Featured = () => {
         <p className="desc">
           Previous transactions processing. Last payments may not be included.
         </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize="small" />
+              <div className="resultAmount">$5.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUp fontSize="small" />
+              <div className="resultAmount">$30k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
